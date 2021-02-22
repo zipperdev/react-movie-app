b899681f919dc08f5d62d1fee6b3ad801fb5c20b
-HAVA- <71908836+ksh1119@users.noreply.github.com>
2021-02-22 10:08:24 +0900

    Update About.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,7 +6,7 @@ function About(props) {
   return (
     <div className="about__container">
       <span>
-        “The Movies Is Always Exiting and Great Witch Made by Humans So It's very Amazing.”
+        “The Movies Is Always Exiting and Great Which Made by Humans So It's very Amazing.”
       </span>
       <span>− Zipper_, 2021</span>
     </div>
